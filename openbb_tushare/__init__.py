ed205001492f401872825065e18acff8535b9fd2
Roger Ye <shugaoye@yahoo.com>
2025-08-09 15:38:26 +0800

    Updated to version 0.3.3

--- openbb_tushare/__init__.py
+++ openbb_tushare/__init__.py
@@ -3,7 +3,7 @@ from mysharelib.tools import setup_logger
 
 project_name = __name__
 
-setup_logger(project_name)
-logger = logging.getLogger(project_name)
+#setup_logger(project_name)
+#logger = logging.getLogger(project_name)
 
-logger.info(f"Current openbb_tushare module name: {project_name}")
+#logger.info(f"Current openbb_tushare module name: {project_name}")
